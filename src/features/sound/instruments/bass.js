c334ec721955a450a252d2de228ce5fe6f4cc95c
Pawitra Warda <pawitrawarda@gmail.com>
2026-05-21 15:03:04 +0700

    fix: update tone bass

--- src/features/sound/instruments/bass.js
+++ src/features/sound/instruments/bass.js
@@ -1,7 +1,8 @@
 import * as Tone from "tone";
 import { getMasterVolume } from "../engine.js";
 
-const BASS_NOTES = [65.41, 73.42, 82.41, 87.31, 98.0, 110.0, 123.47, 130.81];
+// const BASS_NOTES = [65.41, 73.42, 82.41, 87.31, 98.0, 110.0, 123.47, 130.81];
+const BASS_NOTES = [82.41, 87.31, 92.50, 98.0, 103.83, 110.0, 116.54, 123.47];
 
 let synth = null;
 let filter = null;
@@ -11,15 +12,13 @@ let isCreated = false;
 export function createBass() {
   if (isCreated) return;
 
-  filter = new Tone.Filter(250, "lowpass");
-  filter.Q.value = 8;
+  filter = new Tone.Filter(350, "lowpass");
+  filter.Q.value = 12;
 
   const distortion = new Tone.Distortion(0.7);
 
-  const bitcrusher = new Tone.BitCrusher(4);
-
-  const delay = new Tone.FeedbackDelay("4n", 0.4);
-  delay.wet.value = 0.65;
+  const delay = new Tone.FeedbackDelay("16n", 0.4);
+  delay.wet.value = 0.3;
 
   volume = new Tone.Volume(2);
 
@@ -36,8 +35,7 @@ export function createBass() {
     },
   }).connect(distortion);
 
-  distortion.connect(bitcrusher);
-  bitcrusher.connect(filter);
+  distortion.connect(filter);
   filter.connect(delay);
   delay.connect(volume);
   volume.connect(getMasterVolume());
@@ -53,10 +51,10 @@ export function updateBassParams(params) {
   const clampedIndex = Math.max(0, Math.min(7, noteIndex));
   synth.frequency.value = BASS_NOTES[clampedIndex];
 
-  const freq = 200 + params.filter * (800 - 200);
+  const freq = 200 + params.filter * (350 - 200);
   filter.frequency.value = freq;
 
-  const q = ((params.resonance + 1) / 2) * 20;
+  const q = ((params.resonance + 1) / 2) * 12;
   filter.Q.value = q;
 }
 
